a81fd8295350d1826fe1ce65f278ea54c516d6dc
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-25 01:30:19 +0600

    add text and button bg color

--- src/components/Banner/SingleDonation/SingleDonation.jsx
+++ src/components/Banner/SingleDonation/SingleDonation.jsx
@@ -6,8 +6,8 @@ const SingleDonation = ({donation}) => {
         <div className="flex flex-col">
             <img className="" src={picture} alt="" />
             <div className= {`p-4 bg-[${card_bg}] grow`}>
-            <h4 className={`w-20 rounded-lg text-center text-[14px] bg-[${category_bg}]`}>{category}</h4>
-            <h3 className="mt-3 bg-[]">{title}</h3>
+            <h4 className={`w-20 rounded-lg text-center text-[14px] text-[${text_button_bg}] bg-[${category_bg}]`}>{category}</h4>
+            <h3 className={`mt-3 text-[${text_button_bg}]`}>{title}</h3>
             </div>
         </div>
     );
