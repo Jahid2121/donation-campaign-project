7532d698ca54b64b3de481a0943e8fae687e6f7e
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-24 22:10:22 +0600

    Add banner title and searchbar

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 const Banner = () => {
     return (
-        <div className='flex align-middle  items-center'>
-            <h2 >I Grow By Helping People In Need</h2>
+        <div >
+            <h2 className='text-center text-4xl font-bold mt-52'>I Grow By Helping People In Need</h2>
+            <div className='text-center'>
+            <input type="text" className='w-52 h-12'/>
+            <button className='btn mt-8 bg-[#FF444A]'>Search</button>
+            </div>
         </div>
     );
 };
