ef2a4a2d9e3704f753430f2afa08c5b5ee4fc35b
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-25 00:19:22 +0600

    update json and add category bg dynamically

--- src/components/Banner/Donations/Donations.jsx
+++ src/components/Banner/Donations/Donations.jsx
@@ -2,7 +2,7 @@ import SingleDonation from "../SingleDonation/SingleDonation";
 
 const Donations = ({donations}) => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="mt-40 grid grid-cols-1 md:grid-cols-2 gap-3 lg:grid-cols-4">
             {
                 donations?.map(donation => <SingleDonation donation={donation} key={donation.id}></SingleDonation>)
             }
--- src/components/Banner/SingleDonation/SingleDonation.jsx
+++ src/components/Banner/SingleDonation/SingleDonation.jsx
@@ -1,7 +1,7 @@
 
 const SingleDonation = ({donation}) => {
 
-    const {picture, title,category,category_bg,card_bg, text_button_bg, description,price } = donation || {}
+    const {picture, title, category, category_bg, card_bg, text_button_bg, description, price } = donation || {}
     return (
         <div className="relative flex max-w-[24rem] flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
   <div className="relative m-0 overflow-hidden rounded-none bg-transparent bg-clip-border text-gray-700 shadow-none">
@@ -11,12 +11,11 @@ const SingleDonation = ({donation}) => {
     />
   </div>
   <div className="p-6">
-    <h4 className="block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
-      UI/UX Review Check
+    <h4 className={`block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased bg-[${category_bg}]`}>
+      {category}
     </h4>
-    <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700 antialiased">
-      Because it's about motivating the doers. Because I'm here to follow my
-      dreams and inspire others.
+    <p className="mt-3 block font-sans text-xl font-normal leading-relaxed text-gray-700  antialiased">
+      {title}
     </p>
   </div>
   
