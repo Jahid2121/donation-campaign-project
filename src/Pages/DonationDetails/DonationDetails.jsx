d46bcac60935ec68774c251a03feb8ba9b4ac891
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-25 17:44:26 +0600

    show react toast when user click on donate button

--- src/Pages/DonationDetails/DonationDetails.jsx
+++ src/Pages/DonationDetails/DonationDetails.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { useLoaderData, useParams } from "react-router-dom";
 
 const DonationDetails = () => {
@@ -7,7 +8,25 @@ const DonationDetails = () => {
     const {id} = useParams()
     const idInt = parseInt(id)
     const donations = useLoaderData()
-    console.log(donations);
+
+
+    const handleDonation = () => {
+        
+        const donationArray = []
+        
+        const donationList = JSON.parse(localStorage.getItem('donates'))
+        if(!donationList){
+            donationArray.push(donation)
+            localStorage.setItem('donates', JSON.stringify(donationArray))
+            return toast.success('Successfully Donated! Go to donations pages for more information')
+        }
+        else{
+            donationArray.push(...donationList, donation)
+            localStorage.setItem('donates', JSON.stringify(donationArray))
+            return toast.success('Successfully Donated! Go to donations pages for more information')
+        }
+
+    }
 
     useEffect(() => {
         const getDonation = donations?.find(donation => donation.id === idInt)
@@ -18,9 +37,10 @@ const DonationDetails = () => {
     return (
         <div className="ml-40 mt-16 w-[900px]" >
             <img className="w-3/4" src={picture} alt="" />
-            <button className="btn relative flex btn-primary -mt-16 ml-6">Donate ${price}</button>
+            <button onClick={handleDonation} className="btn relative flex btn-primary -mt-16 ml-6">Donate ${price}</button>
             <h4 className="mt-10 text-4xl font-bold">{title}</h4>
             <p>{description}</p>
+            <Toaster></Toaster>
         </div>
     );
 };
