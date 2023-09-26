c90998e2eb38be8567405cb55726e7d62f3492e5
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-26 23:01:19 +0600

    Donation details responsive done

--- src/Pages/DonationDetails/DonationDetails.jsx
+++ src/Pages/DonationDetails/DonationDetails.jsx
@@ -4,7 +4,7 @@ import { useLoaderData, useParams } from "react-router-dom";
 
 const DonationDetails = () => {
     const [donation, setDonation] = useState()
-    const { picture, title, category, category_bg, card_bg, text_button_bg, description, price } = donation || {}
+    const { picture, title, text_button_bg, description, price } = donation || {}
     const {id} = useParams()
     const idInt = parseInt(id)
     const donations = useLoaderData()
@@ -42,11 +42,11 @@ const DonationDetails = () => {
     
     
     return (
-        <div className="ml-40 mt-16 w-[900px] relative" >
-            <div className="bg-blend-overlay">
+        <div className="md:ml-40 ml-8 mt-16 lg:w-[900px] relative" >
+            <div className="bg-blend-overlay ">
             <img className="w-3/4" src={picture} alt="" />
-            <div  className="bg-[#0B0B0B80] w-[675px] h-20 -mt-20 absolute overflow-hidden"></div>
-            <button style={{background: `${text_button_bg}`}} onClick={handleDonation} className="btn relative flex  -mt-16 ml-6">Donate ${price}</button>
+            <div  className="bg-[#0B0B0B80] md:w-[675px] h-20 -mt-20 absolute overflow-hidden"></div>
+            <button style={{background: `${text_button_bg}`}} onClick={handleDonation} className="btn relative text-white flex  -mt-16 ml-6">Donate ${price}</button>
             <h4 className="mt-10 text-4xl font-bold">{title}</h4>
             </div>
 
--- src/components/Banner/SingleDonation/SingleDonation.jsx
+++ src/components/Banner/SingleDonation/SingleDonation.jsx
@@ -10,9 +10,9 @@ const SingleDonation = ({donation}) => {
         <div className="flex flex-col ">
             <img className="" src={picture} alt="" />
             <div style={{background: `${card_bg}`}} className="p-4  grow">
-            <h4 style={{background: `${category_bg}`, color: `${text_button_bg}`}}  className=" w-20 rounded-lg text-center text-[14px]">{category}</h4>
+            <h4 style={{background: `${category_bg}`, color: `${text_button_bg}`}}  className=" w-20 rounded-lg text-center text-[14px] font-medium">{category}</h4>
 
-            <h3 style={{color: `${text_button_bg}`}} className="mt-3" >{title}</h3>
+            <h3 style={{color: `${text_button_bg}`}} className="mt-3 font-semibold" >{title}</h3>
             </div>
         </div>
         </Link>
