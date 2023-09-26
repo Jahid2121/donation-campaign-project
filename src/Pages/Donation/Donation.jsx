0c024f0c068bf8c958d23e244a7b2a2e1bfac124
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-26 07:19:51 +0600

    Add show all button for show all data in donation page

--- src/Pages/Donation/Donation.jsx
+++ src/Pages/Donation/Donation.jsx
@@ -4,7 +4,7 @@ import DonatedList from "../../components/DonatedList/DonatedList";
 const Donation = () => {
     const [noDonationData, setNoDonationData] = useState(false)
     const [donated, setDonated] = useState([])
-    
+    const [showAll, setShowAll] = useState(false)
     useEffect(() => {
         const donationList = JSON.parse(localStorage.getItem('donates'))
         if(donationList){
@@ -13,7 +13,6 @@ const Donation = () => {
         else{
             setNoDonationData('Not Donated yet!')
         }
-
     },[])
     return (
         <div>
@@ -21,12 +20,22 @@ const Donation = () => {
             {noDonationData ? <p className="flex items-center justify-center text-center h-[80vh]">{noDonationData}</p>
                 :
                 <div>
+                    <div className="grid grid-cols-2">
                 {
+                    showAll ? 
                     donated?.map(donation => <DonatedList key={donation.id} donation={donation}></DonatedList>)
+                    :
+                    donated?.slice(0, 4).map(donation => <DonatedList key={donation.id} donation={donation}></DonatedList>)
                 }
+                </div>
+                 {
+                    donated.length > 4 && !showAll ? <button onClick={() => setShowAll(!showAll)} className='flex justify-center items-center text-center btn bg-green-500'>See All</button> : ''
+                    
+                 }
                 </div>
             }
             </div>
+            
         </div>
     );
 };
--- src/components/DonatedList/DonatedList.jsx
+++ src/components/DonatedList/DonatedList.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
 const DonatedList = ({donation}) => {
-    console.log(donation);
     const { picture, title, category, category_bg, card_bg, text_button_bg, description, price } = donation || {}
     return (
         <div>
-            
-                <img src={picture} alt="" />
-            
+               <div className='flex'>
+               <img className='w-1/2' src={picture} alt="" />
+               <div className='flex flex-col'>
+               <span>
+                    {category}
+                   
+                </span>
+                <span>
+                {title}
+                </span>
+                <span>
+                    ${price}
+                </span>
+                <span>
+                    <button>View Details</button>
+                </span>
+               </div>
+               </div>
+          
         </div>
     );
 };
