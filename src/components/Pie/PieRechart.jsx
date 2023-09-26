9ea6f81342ac26d723b4049f18b943ccd53220ff
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-26 14:12:40 +0600

    Show percentage in the pie

--- src/components/Pie/PieRechart.jsx
+++ src/components/Pie/PieRechart.jsx
@@ -1,14 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { Cell, Pie, PieChart } from 'recharts';
 
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+  const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+  const x = cx + radius * Math.cos(-midAngle * RADIAN);
+  const y = cy + radius * Math.sin(-midAngle * RADIAN); 
+
+
+
+  return (
+    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+      {`${(percent * 100).toFixed(0)}%`}
+    </text>
+  );
+
+}
+
 const PieRechart = () => {
-    const [donated, setDonated] = useState([])
     const [chartData, setChartData] = useState( )
     
     
     useEffect(() => {
         const donationList = JSON.parse(localStorage.getItem('donates'))
-        setDonated(donationList)
 
         const total = 12;
     const totalDonated = donationList ? donationList.length : 0 ;
@@ -31,15 +45,16 @@ const PieRechart = () => {
             cy="50%"
             labelLine={false}
             outerRadius={80}
-            fill="#8884d8"
             dataKey="value"
             nameKey="name"
+            label={renderCustomizedLabel}
           >
             
           <Cell fill='#FF444A'/>
           <Cell fill='#00C49F'/>
            
           </Pie>
+          
         </PieChart>
         </div>
     );
