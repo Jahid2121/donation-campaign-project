d405fd8158e5966bc5bbef63542ff2e46480f995
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-27 12:28:35 +0600

    fix another issue of donations page

--- src/components/DonatedList/DonatedList.jsx
+++ src/components/DonatedList/DonatedList.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const DonatedList = ({donation}) => {
     const { picture, title, category, category_bg, card_bg, text_button_bg, id } = donation || {}
     return (
-        <div className="flex mx-auto">
+        <div className="flex">
             <img className="w-1/3" src={picture} alt="" />
             <div style={{background: `${card_bg}`}} className="p-4 grow">
             <h4 style={{background: `${category_bg}`, color: `${text_button_bg}`}}  className=" w-20 rounded-lg text-center font-normal text-[14px]">{category}</h4>
