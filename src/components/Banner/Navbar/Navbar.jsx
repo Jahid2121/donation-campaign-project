4626d50cd0341319df5d4bda3af366c65381763b
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-27 06:06:45 +0600

    Logo image imported

--- src/components/Banner/Navbar/Navbar.jsx
+++ src/components/Banner/Navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import { NavLink } from "react-router-dom";
-
+import image from '../../../assets/Logo.png'
 const Navbar = () => {
   return (
     <div className="">
       <nav className="flex flex-col md:flex-row justify-between md:mr-12 mt-14 md:mt-12">
-        <img className="w-1/4 md:ml-20 ml-32" src="/src/assets/Logo.png" alt="" />
+        <img className="w-1/4 md:ml-20 ml-32" src={image} alt="" />
         <ul className="flex ml-9 gap-10 mt-6">
           <li>
             <NavLink
