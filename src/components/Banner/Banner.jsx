deb09022f7a3556c1e1f596ef36b990f43c581ea
Jahid2121 <jahidsarkar2121@gmail.com>
2023-09-27 06:54:09 +0600

    Fix reponsive issue of navbar

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,16 +1,15 @@
-import { useState } from "react";
 import '../Banner/Navbar/Navbar.css'
-const Banner = ({donations, find, setFind, setSearch ,search}) => {
+const Banner = ({ setFind, setSearch ,search}) => {
     const handleSearch = () => {
         setSearch(!search)
     }
     return (
-        <div className='overlay p-52 bg-opacity-50 container mx-auto'>
+        <div className='overlay p-3 md:p-52 bg-opacity-50 container mx-auto'>
             <h2 className='text-center text-4xl font-bold '>I Grow By Helping People In Need</h2>
             <div className='text-center '>
 
 
-            <input placeholder="Search here..." type="text" className='w-72 border-2 focus:p-3 rounded-l-lg h-12 pl-4' onChange={e=> setFind(e.target.value)}/>
+            <input placeholder="Search here..." type="text" className=' md:w-72 border-2 focus:p-3 rounded-l-lg h-12 pl-4' onChange={e=> setFind(e.target.value)}/>
             <button onClick={handleSearch} className='p-3
             rounded-r-lg mt-8 bg-[#FF444A] text-white'>Search</button>
             </div>
--- src/components/Banner/Navbar/Navbar.jsx
+++ src/components/Banner/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import image from '../../../assets/Logo.png'
 const Navbar = () => {
   return (
     <div className="">
-      <nav className="flex flex-col md:flex-row justify-between md:mr-12 mt-14 md:mt-12">
+      <nav className="flex flex-col mb-10 md:mb-2 md:flex-row justify-between md:mr-12 mt-14 md:mt-12">
         <img className="w-1/4 md:ml-20 ml-32" src={image} alt="" />
         <ul className="flex ml-9 gap-10 mt-6">
           <li>
